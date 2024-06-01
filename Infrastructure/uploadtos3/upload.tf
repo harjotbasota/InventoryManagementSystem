a7540d8020205e47d8638ba3f0068cea3f822668
harjotbasota <harjotsinghbasota@gmail.com>
2024-06-01 01:03:21 -0400

    project

--- Infrastructure/uploadtos3/upload.tf
+++ Infrastructure/uploadtos3/upload.tf
@@ -1,34 +1,34 @@
-resource "aws_s3_object" "index.html" {
+resource "aws_s3_object" "index" {
   bucket = data.terraform_remote_state.s3.outputs.bucket_name
   key    = "index.html"
   source = "../../frontend/index.html"
   content_type = "text/html"
 }
-resource "aws_s3_object" "delete.html" {
+resource "aws_s3_object" "delete" {
   bucket = data.terraform_remote_state.s3.outputs.bucket_name
   key    = "delete.html"
   source = "../../frontend/delete.html"
   content_type = "text/html"
 }
-resource "aws_s3_object" "get.html" {
+resource "aws_s3_object" "get" {
   bucket = data.terraform_remote_state.s3.outputs.bucket_name
   key    = "get.html"
   source = "../../frontend/get.html"
   content_type = "text/html"
 }
-resource "aws_s3_object" "getfiltered.html" {
+resource "aws_s3_object" "getfiltered" {
   bucket = data.terraform_remote_state.s3.outputs.bucket_name
   key    = "getfiltered.html"
   source = "../../frontend/getfiltered.html"
   content_type = "text/html"
 }
-resource "aws_s3_object" "update.html" {
+resource "aws_s3_object" "update" {
   bucket = data.terraform_remote_state.s3.outputs.bucket_name
   key    = "update.html"
   source = "../../frontend/update.html"
   content_type = "text/html"
 }
-resource "aws_s3_object" "upload.html" {
+resource "aws_s3_object" "upload" {
   bucket = data.terraform_remote_state.s3.outputs.bucket_name
   key    = "upload.html"
   source = "../../frontend/upload.html"
