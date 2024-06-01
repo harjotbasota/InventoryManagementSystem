2f427c6ed78f39cbc02dde48da00861ab4e96526
harjotbasota <harjotsinghbasota@gmail.com>
2024-06-01 01:25:00 -0400

    project

--- Infrastructure/apigateway/outputs.tf
+++ Infrastructure/apigateway/outputs.tf
@@ -6,5 +6,5 @@ output "hosted_ui_url"{
 }
 
 output "logout_url"{
-   value = "https://${aws_cognito_user_pool_domain.domain.domain}.auth.us-east-1.amazoncognito.com/logout?client_id=${aws_cognito_user_pool_client.myclient.id}&redirect_uri=${urlencode("https://${data.terraform_remote_state.s3.outputs.bucket_name}.s3.amazonaws.com/index.html")}"
+   value = "https://${aws_cognito_user_pool_domain.domain.domain}.auth.us-east-1.amazoncognito.com/logout?client_id=${aws_cognito_user_pool_client.myclient.id}&response_type=token&redirect_uri=${urlencode("https://${data.terraform_remote_state.s3.outputs.bucket_name}.s3.amazonaws.com/index.html")}"
 }
